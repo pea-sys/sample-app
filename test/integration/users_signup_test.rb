03f203f93d1b892de958c5c811b229abd9b3f6a2
pea-sys <pea98258@gmail.com>
2024-06-18 05:47:01 +0900

    11-2 Add account activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,8 +10,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "password" } }
     end
     follow_redirect!
-    assert_template 'users/show'
-    assert is_logged_in?
+    #assert_template 'users/show'
+    #assert is_logged_in?
   end
 
   test "invalid signup information" do
